ff94d9e55be58d5058cb9741b5323d5fe00568e0
Zerg009 <semenencostanislav@gmail.com>
2023-05-27 13:13:36 +0300

    intial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpHeight;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool isGrounded;
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
+        // Flip player when moving left/right
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one * 6;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1) * 6;
+        }
+
+        // Set animator parameters
+        //if (horizontalInput > 0.01 || horizontalInput < -0.01f)
+        //    anim.SetBool("isRunning", true);
+        //else
+        //    anim.SetBool("isRunning", false);
+        anim.SetBool("isRunning", horizontalInput != 0);
+        anim.SetBool("isGrounded", isGrounded);
+    }
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpHeight);
+        isGrounded = false;
+        anim.SetTrigger("Jump");
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = true;
+    }   
+}
